85d9edcd37fbeaa1fdee269e5f935fbb2de7edf5
isengupt <ishan6060@gmail.com>
2020-12-01 15:34:10 -0500

    Updates

---
+++ precache-manifest.32c276827418f4d7c75947a9637b48e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e42a915a149e2ae9e2bdacba61f84a5",
+    "url": "/music-player/index.html"
+  },
+  {
+    "revision": "42dab84ce655dbf7abc5",
+    "url": "/music-player/static/css/main.f07cdaf3.chunk.css"
+  },
+  {
+    "revision": "ca24ab308c7fbdf6ea71",
+    "url": "/music-player/static/js/2.2d0e7d61.chunk.js"
+  },
+  {
+    "revision": "ab4d9979552be1c73ac7",
+    "url": "/music-player/static/js/3.94930ddb.chunk.js"
+  },
+  {
+    "revision": "42dab84ce655dbf7abc5",
+    "url": "/music-player/static/js/main.78e63254.chunk.js"
+  },
+  {
+    "revision": "fd43b0f6de2d3dada67f",
+    "url": "/music-player/static/js/runtime-main.20b7bbf2.js"
+  },
+  {
+    "revision": "977769bf28e93a97d5c283c3531652c3",
+    "url": "/music-player/static/media/GummyBearz.977769bf.mp3"
+  },
+  {
+    "revision": "48f1f9fe8edb5ba87d94d505407ec3ed",
+    "url": "/music-player/static/media/dot.48f1f9fe.png"
+  }
+]);
